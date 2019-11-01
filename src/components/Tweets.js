68e5178d11f5d73c887b81e72b5e7b508af945d3
Brooke Lewis <belv2c@gmail.com>
2019-11-01 14:58:13 -0500

    initial commit and crud functionality

---
+++ src/components/Tweets.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Tweet from './Tweet'
+import CreateTweet from './CreateTweet'
+import Query from './Query'
+import { QUERY_GET_TWEETS } from '../graphql/queries'
+import './Tweets.css'
+
+class Tweets extends React.Component {
+  render() {
+    return (
+      <div className="tweets">
+        <CreateTweet />
+        <Query query={QUERY_GET_TWEETS} render={Tweet} />
+      </div>
+    )
+  }
+}
+
+export default Tweets
